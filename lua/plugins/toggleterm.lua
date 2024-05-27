68c4f409566e933f8e52bb3648fb528f13222132
Asif Mohammad Mollah <admin@mrasif.in>
2024-05-27 23:59:28 +0400

    chore: toggleterm added

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+return {
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require("toggleterm").setup({
+      size = 120,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = false,
+      shell = vim.o.shell,
+      float_opts = {
+          border = "curved",
+          winblend = 0,
+          highlights = {
+              border = "Normal",
+              background = "Normal",
+          },
+      },
+    })
+
+    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], {silent = true})
+  end,
+}
